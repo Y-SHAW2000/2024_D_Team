833e80a4fb78cc119acd55afecf7e2ca1677d1c1
Y-SHAW2000 <yshaw0107@gmail.com>
2024-10-29 13:33:00 +0900

    ワープを追加

---
+++ Assets/PortalTarget.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTarget : MonoBehaviour
+{
+    public Transform target;
+
+    public Transform GetTarget()
+    {
+        return target;
+    }
+}
